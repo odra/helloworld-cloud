031a5b44830c507ad65a9a90c7315e15e9c230ee
Matthias Wessendorf <matzew@apache.org>
2016-03-07 15:57:52 +0100

    Making the push cloud app use the .push() API

--- application.js
+++ application.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 // Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-app.use('/hello', require('./lib/hello.js')());
+app.use('/push', require('./lib/push.js')());
 
 // Important that this is last!
 app.use(mbaasExpress.errorHandler());
@@ -30,5 +30,5 @@ app.use(mbaasExpress.errorHandler());
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+  console.log("App started at: " + new Date() + " on port: " + port);
 });
